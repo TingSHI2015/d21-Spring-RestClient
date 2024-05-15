1039e6ba49fc9f395906febc9dbcc638e083d481
tingSHI <ting.shi2015@gmail.com>
2024-05-15 10:43:20 +0200

    set the project

---
+++ src/main/java/com/github/tingshi2015/d21springrestclient/D21SpringRestClientApplication.java
@@ -0,0 +1,13 @@
+package com.github.tingshi2015.d21springrestclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class D21SpringRestClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(D21SpringRestClientApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/tingshi2015/d21springrestclient/D21SpringRestClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.tingshi2015.d21springrestclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class D21SpringRestClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
